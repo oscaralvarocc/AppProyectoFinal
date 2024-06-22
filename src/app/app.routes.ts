6eb77c130c875fae25040a445d55220334acab04
oscaralvarocc <oscaralvarocoronadocruz@gmail.com>
2024-06-21 22:14:00 -0400

    Actividad 2 de Proyecto Final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: `acerca-de`,component:AcercaDeComponent},
+    {path: `inicio`,component:InicioComponent}
+];
